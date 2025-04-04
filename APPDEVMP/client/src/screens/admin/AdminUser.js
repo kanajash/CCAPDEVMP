83d2b8d0ddf3826b6a4c26955c563d582320a535
kanajash <141270976+kanajash@users.noreply.github.com>
2025-04-05 01:24:13 +0800

    Update AdminUser.js

--- APPDEVMP/client/src/screens/admin/AdminUser.js
+++ APPDEVMP/client/src/screens/admin/AdminUser.js
@@ -19,7 +19,7 @@ function AdminUser() {
     useEffect(() => {
         const getUser = async () => {
             try {
-                const res = (await axios.get(`http://localhost:5000/api/users/${id}`)).data
+                const res = (await axios.get(`https://backend-k86c.onrender.com/`)).data
                 setUsername(res.name)
                 setEmail(res.email)
                 setIsAdmin(res.isAdmin)
