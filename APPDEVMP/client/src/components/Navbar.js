2b5483528513f8df0de2ac046066edd4c86853dc
kanajash <141270976+kanajash@users.noreply.github.com>
2025-04-05 02:37:42 +0800

    Update Navbar.js

--- APPDEVMP/client/src/components/Navbar.js
+++ APPDEVMP/client/src/components/Navbar.js
@@ -1,18 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { NavDropdown } from 'react-bootstrap'
-import {FaUser} from "react-icons/fa6"
+import { FaUser } from "react-icons/fa6"
+
 export default function Navbar() {
 
-  // Getting User From Local Storage
   const user = JSON.parse(localStorage.getItem("currentUser"))
 
-  // Removing User From Local Storage
   const logOut = () => {
     localStorage.removeItem("currentUser");
     window.location.href = "/login"
   }
-
 
   return (
     <>
@@ -20,25 +18,30 @@ export default function Navbar() {
         <div className="container-fluid">
           <a className="navbar-brand" href="/">Co-Working Space</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon " ></span>
+            <span className="navbar-toggler-icon"></span>
           </button>
+
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0" style={{ marginRight: "8%" }}>
-              {/* If User is Available Ternary Operator is used */}
+
+              {/* Always show About */}
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">About</Link>
+              </li>
+
               {user ? (
                 <>
-
-                  <NavDropdown title={<FaUser/>} id="basic-nav-dropdown" className='mx-2'>
-                   
-                    <NavDropdown.Item ><Link to={"/bookings"} className='text-dark text-decoration-none'>Profile</Link></NavDropdown.Item>
+                  <NavDropdown title={<FaUser />} id="basic-nav-dropdown" className='mx-2'>
+                    <NavDropdown.Item>
+                      <Link to="/bookings" className='text-dark text-decoration-none'>Profile</Link>
+                    </NavDropdown.Item>
                     <NavDropdown.Divider />
                     <NavDropdown.Item onClick={logOut}>
-                      Logut
+                      Logout
                     </NavDropdown.Item>
                   </NavDropdown>
-
-
-                </>) : (
+                </>
+              ) : (
                 <>
                   <li className="nav-item">
                     <Link className="nav-link" to="/register">Register</Link>
@@ -46,32 +49,27 @@ export default function Navbar() {
                   <li className="nav-item">
                     <Link className="nav-link" to="/login">Login</Link>
                   </li>
-
-
-                </>)
-              }
+                </>
+              )}
 
               {user?.isAdmin &&
-                <NavDropdown title={"Admin"} id="basic-nav-dropdown" className='mx-2'>
-                   
-                   <NavDropdown.Item ><Link to={"/admin/bookings"} className='text-dark text-decoration-none'>Bookings</Link></NavDropdown.Item>
-                   <NavDropdown.Item ><Link to={"/admin/rooms"} className='text-dark text-decoration-none'>Rooms</Link></NavDropdown.Item>
-                   <NavDropdown.Item ><Link to={"/admin/users"} className='text-dark text-decoration-none'>Users</Link></NavDropdown.Item>
-
-              
-              </NavDropdown>
+                <NavDropdown title="Admin" id="basic-nav-dropdown" className='mx-2'>
+                  <NavDropdown.Item>
+                    <Link to="/admin/bookings" className='text-dark text-decoration-none'>Bookings</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link to="/admin/rooms" className='text-dark text-decoration-none'>Rooms</Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Item>
+                    <Link to="/admin/users" className='text-dark text-decoration-none'>Users</Link>
+                  </NavDropdown.Item>
+                </NavDropdown>
               }
 
             </ul>
-
           </div>
         </div>
       </nav>
-
-
-
-
-
     </>
   )
 }
