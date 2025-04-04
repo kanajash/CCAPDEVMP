0035dfb38c15e731cf72d9baa252eb5331c709ce
kanajash <141270976+kanajash@users.noreply.github.com>
2025-04-05 01:23:28 +0800

    Update AdminBookings.js

--- APPDEVMP/client/src/screens/admin/AdminBookings.js
+++ APPDEVMP/client/src/screens/admin/AdminBookings.js
@@ -14,7 +14,7 @@ function AdminBookings() {
     const getBookings = async () => {
     try {
       setLoading(true)
-      const res = await axios.get("http://localhost:5000/api/bookings/")
+      const res = await axios.get("https://backend-k86c.onrender.com/")
       setBookings(res.data)
       setLoading(false)
     } catch (error) {
