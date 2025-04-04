638963521d8c1baa4aebda422bb0723aceb55c77
kanajash <141270976+kanajash@users.noreply.github.com>
2025-04-05 02:37:05 +0800

    Update App.js

--- APPDEVMP/client/src/App.js
+++ APPDEVMP/client/src/App.js
@@ -1,19 +1,19 @@
 import './App.scss';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import Navbar from './components/Navbar';
 import Homescreen from './screens/Homescreen';
 import {
   BrowserRouter,
   Route,
   Routes,
-} from "react-router-dom"
+} from "react-router-dom";
 import Bookingscreen from './screens/Bookingscreen';
 import Registerscreen from './screens/Registerscreen';
 import Loginscreen from './screens/Loginscreen';
 import Profile from './screens/ProfileScreen';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
-import "react-toastify/ReactToastify.min.css"
+import "react-toastify/ReactToastify.min.css";
 import AdminRoute from './components/AdminRoute';
 import AdminBookings from './screens/admin/AdminBookings';
 import AdminRooms from './screens/admin/AdminRooms';
@@ -21,40 +21,36 @@ import AdminUsers from './screens/admin/AdminUsers';
 import AdminUser from './screens/admin/AdminUser';
 import AdminRoom from './screens/admin/AdminRoom';
 import AdminBookingDetails from './screens/admin/AdminBookingDetails';
+import About from './screens/About';
+
 function App() {
   return (
     <>
-
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/' exact Component={Homescreen} />
-          <Route path='/register' exact Component={Registerscreen} />
-          <Route path='/login' exact Component={Loginscreen} />
+          <Route path='/' element={<Homescreen />} />
+          <Route path='/register' element={<Registerscreen />} />
+          <Route path='/login' element={<Loginscreen />} />
+          <Route path='/about' element={<About />} />
+
           <Route element={<PrivateRoute />}>
-            <Route path='/bookings' exact Component={Profile} />
-            <Route path='/book/:roomid/:fromDate/:toDate' exact Component={Bookingscreen} />
-          </Route>
-          <Route element={<AdminRoute/>}>
-          <Route path='/admin/bookings' exact Component={AdminBookings} />
-          <Route path='/admin/booking/details/:id' exact Component={AdminBookingDetails} />
-          <Route path='/admin/rooms' exact Component={AdminRooms} />
-          <Route path='/admin/rooms/edit/:id' exact Component={AdminRoom} />
-          <Route path='/admin/users' exact Component={AdminUsers} />
-          <Route path='/admin/users/edit/:id' exact Component={AdminUser} />
-          
+            <Route path='/bookings' element={<Profile />} />
+            <Route path='/book/:roomid/:fromDate/:toDate' element={<Bookingscreen />} />
           </Route>
 
-
-
+          <Route element={<AdminRoute />}>
+            <Route path='/admin/bookings' element={<AdminBookings />} />
+            <Route path='/admin/booking/details/:id' element={<AdminBookingDetails />} />
+            <Route path='/admin/rooms' element={<AdminRooms />} />
+            <Route path='/admin/rooms/edit/:id' element={<AdminRoom />} />
+            <Route path='/admin/users' element={<AdminUsers />} />
+            <Route path='/admin/users/edit/:id' element={<AdminUser />} />
+          </Route>
         </Routes>
+
         {/* <Footer /> */}
       </BrowserRouter>
-
-
-
-
-
     </>
   );
 }
